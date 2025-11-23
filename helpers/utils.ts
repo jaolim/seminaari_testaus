ae922094cb1c06fce6e267402e349f3db350c6e6
Janne <bhi256@myy.haaga-helia.fi>
2025-11-23 18:08:05 +0200

    Indent fix

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -2,8 +2,8 @@ import { Page } from '@playwright/test';
 
 export async function trClickIfExists(page: Page, row: string, element: string) {
     const clickable = page
-    .locator('tr', { hasText: row })
-    .getByRole('link', { name: element })
+        .locator('tr', { hasText: row })
+        .getByRole('link', { name: element })
 
     if (await clickable.count()) {
         await clickable.click();
